e6365ab9ceee3436debcd4be46a19b5152e3f487
Basant <chbasantmalik@gmail.com>
2026-08-24 09:39:48 +0530

    JS

--- promise.js
+++ promise.js
@@ -1,18 +1,42 @@
-const promise = new Promise((resolve,reject)=>{
-    let success = true
-    if(success){
+const promise1 = new Promise((resolve, reject) => {
+    let success = true;
+    if (success) {
         resolve({
-            id: 2930309,
-            username: "john doe"
+            id: 73676463866487,
+            username: 'John Doe'
         })
-    }else{
-        reject(new Error("data not fetched"))
+    }
+    else {
+        reject(new Error("Data not Fetched"))
     }
 })
 
-promise.then((response)=>{
-    console.log(response);
-}).catch((error)=>{
-    console.log(error.message);
-});
 
+promise1.then(response => console.log(response)).catch(error => console.log(error.message))
+
+
+
+
+const promise2 = new Promise((resolve, reject) => {
+    let success = true;
+    if (success) {
+        resolve({
+            id: 73676463866487,
+            username: 'John Doe'
+        })
+    }
+    else {
+        reject(new Error("Data not Fetched"))
+    }
+})
+
+
+promise2.then(response => console.log(response)).catch(error => console.log(error.message))
+
+Promise.all([promise1, promise2])
+    .then((responses) => {
+        console.log(responses);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
