643b19c063ef093490645c627087e78e94abfe29
Basant <chbasantmalik@gmail.com>
2026-08-25 11:59:11 +0530

    JS

--- event.js
+++ event.js
@@ -4,4 +4,14 @@ setTimeout(() => {
     console.log("setTimeout");
 }, 3000);
 
-fs.readFile("intro.txt")
+fs.readFile("intro.txt","utf8",(err,data)=>{
+    console.log("file read completed");
+})
+
+setInterval(()=>{
+    console.log("set interval after 5ms");
+},500);
+
+setImmediate(()=>{
+    console.log("set immediate");
+})
