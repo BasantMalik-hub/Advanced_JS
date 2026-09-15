9a31decdb5e9ee2a2f9a391e7f8abd7a886841ed
Basant <chbasantmalik@gmail.com>
2026-09-15 11:47:47 +0530

    js

---
+++ http_module/basic_server.js
@@ -0,0 +1,38 @@
+import http from 'http';
+const fs = require('fs')
+
+const htmlData = fs.readFileSync('./index.html','utf-8');
+
+
+const server = http.createServer((req, res) => {
+    console.log("hello");
+    const order = {
+        orderId: 10987,
+        des:"Delhi",
+        source:"Ghaziabad",
+        username:"ABC"
+    }
+    res.writeHead(200,{
+        "content-type": "application/json",
+        "custom-header": "Hello ECE"
+    })
+    res.end(JSON.stringify(order))
+    // res.statusCode = 200;
+    // res.setHeader("content-type","application/json")
+    // res.end("Hello Everyone");
+});
+server.listen(3000,"127.0.0.1", () => {
+    console.log("server is running on http://127.0.0.1:3000/..");
+});
+
+
+
+//status code 200 means everything is ok
+//status code 201 means something is created
+//status code 401 means unauthorized
+//status code 403 means forbidden
+//status code 404 means not found
+//status code 500 means internal server error
+//status code 503 means service unavailable
+
+// we can send plain(text),json,html
